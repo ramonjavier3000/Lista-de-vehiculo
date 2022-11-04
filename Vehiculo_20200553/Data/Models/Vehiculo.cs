70c9e78666106fc601ff6db7f99c87ce66ef61bc
ramonjavier3000 <117464036+ramonjavier3000@users.noreply.github.com>
2022-11-04 11:06:51 -0400

    Add files via upload

---
+++ Vehiculo_20200553/Data/Models/Vehiculo.cs
@@ -0,0 +1,10 @@
+namespace Vehiculo_20200553.Data.Models;
+public class vehiculo
+{
+    public string? VehiculoID { get; set; }
+    public string? Marca { get; set; }
+    public string? Modelo { get; set; }
+    public string? Año { get; set; }
+    public string? Color { get; set; }
+    
+}
